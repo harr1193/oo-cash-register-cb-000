6e0aaeb36dea90db809a85c6234bb835bd804377
Kyle Harris <k_harris95@live.com>
2018-03-28 02:26:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
-    @last_item = [price, quantity]
+    self.last_item = [price, quantity]
     self.total += price * quantity
     quantity.times do
       items << title
